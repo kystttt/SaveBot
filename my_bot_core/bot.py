35d7a05da1969be11faf223d534a37ecf7ffc4a4
Sevostianov Leonid <doghoper75@gmail.com>
2025-06-22 03:49:09 +0500

    Обновлено bot.py: добавлена поддержка видео с youtube/youtube shorts

--- my_bot_core/bot.py
+++ my_bot_core/bot.py
@@ -17,7 +17,9 @@ app_telegram.add_handler(
          filters.Regex("^https:\/\/vt\.tiktok\.com.*\/S*") |
          filters.Regex("^https:\/\/vkvideo\.ru.*") |
          filters.Regex("^https:\/\/vk\.com\/video.*") |
-          filters.Regex("^https:\/\/vk\.com\/clip.*")) &
+         filters.Regex("^https:\/\/vk\.com\/clip.*") |
+         filters.Regex("^https:\/\/www\.youtube\.com.*") |
+         filters.Regex("^https:\/\/youtube\.com.*")) &
         (~filters.UpdateType.EDITED), logic.reply_video))
 app_telegram.add_handler(
     MessageHandler(
